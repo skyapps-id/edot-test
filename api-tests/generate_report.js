e18d860b62b6d1b63fac5bd946ed0925e2974c3f
Ajii <ajiedafuq@gmail.com>
2025-05-20 19:49:32 +0700

    feat(api-tests) generate report

---
+++ api-tests/generate_report.js
@@ -0,0 +1,11 @@
+const reporter = require('cucumber-html-reporter');
+
+const options = {
+  theme: 'bootstrap',
+  jsonFile: 'report/cucumber_report.json',
+  output: 'report/cucumber_report.html',
+  reportSuiteAsScenarios: true,
+  launchReport: true,
+};
+
+reporter.generate(options);
